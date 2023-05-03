d2b63e2d355d1dad4cd3db37006fcbd29c6a2702
VyachikCh <slawickchuchupal@yandex.ru>
2023-05-03 09:58:17 +0800

    up0305

--- jamaMa.java
+++ jamaMa.java
@@ -5,46 +5,59 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class jamaMa {
+    public static int rows, columns;
     public static void main(String[] args) {
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("testJaMa.txt"));
-            String line = reader.readLine();
-            String[] size = line.split("\\s+");
 
-            int rows = Integer.parseInt(size[0]);
-            int columns = Integer.parseInt(size[1]);
+            String fileName1 = "testJaMa1.txt";
+            double[][] data1 = fileToMatrix(fileName1);
+            Matrix matrix1 = new Matrix(data1);
 
-            double[][] data = new double[rows][columns];
+            String fileName2 = "testJaMa2.txt";
+            double[][] data2 = fileToMatrix(fileName2);
+            Matrix matrix2 = new Matrix(data2);
 
-            for (int i = 0; i < rows; i++) {
-                line = reader.readLine();
-                String[] elements = line.split("\\s+");
-                for (int j = 0; j < columns; j++) {
-                    data[i][j] = Double.parseDouble(elements[j]);
-                }
-            }
+            Matrix matrixSum = matrix1.plus(matrix2);
 
-            reader.close();
+            matrixSum.print(4, 1); // вывод матрицы в консоль для проверки
 
-            Matrix matrix = new Matrix(data);
-            Matrix multipliedMatrix = matrix.times(2); // умножение матрицы на 2
-
-            // вывод матрицы для проверки
-            multipliedMatrix.print(4, 2);
-
-            // создание нового файла и запись в него умноженной матрицы
-            FileWriter writer = new FileWriter("resultJaMa.txt");
-            writer.write(rows + " " + columns + "\n");
-            for (int i = 0; i < rows; i++) {
-                for (int j = 0; j < columns; j++) {
-                    writer.write(multipliedMatrix.get(i, j) + " ");
-                }
-                writer.write("\n");
-            }
-            writer.close();
+            jamaMa obj = new jamaMa();
+            obj.matrixToFile(matrixSum);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    public static double[][] fileToMatrix(String fileName) throws IOException{
+
+        BufferedReader reader = new BufferedReader(new FileReader(fileName)); // открытие файла для чтения
+            String line = reader.readLine(); // чтение первой строки файла
+            String[] size = line.split("\\s+"); // разбиение строки на массив строк, используя пробелы как разделители
+            rows = Integer.parseInt(size[0]); // количество строк в матрице
+            columns = Integer.parseInt(size[1]); // количество столбцов в матрице
+            double[][] data = new double[rows][columns]; // создание двумерного массива для хранения данных из файла
+            for (int i = 0; i < rows; i++) { // цикл по строкам матрицы
+                line = reader.readLine(); // чтение строки из файла
+                String[] elements = line.split("\\s+"); // разбиение строки на массив строк, используя пробелы как разделители
+                for (int j = 0; j < columns; j++) { // цикл по столбцам матрицы
+                    data[i][j] = Double.parseDouble(elements[j]); // преобразование элемента из строки в тип double и добавление его в матрицу
+                }
+            }
+            reader.close(); // закрытие файла
+
+        return data;
+    }
+
+    public static void matrixToFile(Matrix matrixSum) throws IOException{ 
+
+        FileWriter writer = new FileWriter("resultJaMa.txt"); // создание нового файла для записи
+        writer.write(rows + " " + columns + "\n"); // запись размерности матрицы в первую строку файла
+        for (int i = 0; i < rows; i++) { // цикл по строкам матрицы
+            for (int j = 0; j < columns; j++) { // цикл по столбцам матрицы
+                writer.write(matrixSum.get(i, j) + " "); // запись элемента матрицы в файл
+            }
+            writer.write("\n"); // переход на новую строку
+        }
+        writer.close(); // закрытие файла
     }
 }
